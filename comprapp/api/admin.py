5a635daeeb21906ee7de9b7ffd56fd08e959b0d6
pablodt <pablo.duran.t@uma.es>
2024-06-17 17:55:47 +0200

    Register models in admin.py

--- comprapp/api/admin.py
+++ comprapp/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import PurchasingUser, House, Membership, SuperMarket, Product
 
 # Register your models here.
+
+admin.site.register(PurchasingUser)
+admin.site.register(House)
+admin.site.register(Membership)
+admin.site.register(SuperMarket)
+admin.site.register(Product)
